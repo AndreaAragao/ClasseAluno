08c4e322455a4307b2e7e3993d40c28273fcccad
Andréa <andreamaragao@gmail.com>
2023-03-13 22:40:19 -0300

    Adicionar arquivos de projeto.

---
+++ App/Aluno.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto06.App
+{
+    internal class Aluno
+    {
+        public double Nota1 { get; private set; }
+
+        public double Nota2 { get; private set; }
+
+        public double Nota3 { get; private set; }
+
+        public Aluno(double nota1, double nota2, double nota3)
+        {
+            Nota1 = nota1;
+            Nota2 = nota2;
+            Nota3 = nota3;
+        }
+
+        public double mediaNotas()
+        {
+            double mediaNotas = (Nota1 + Nota2 + Nota3) / 3;
+            return mediaNotas;
+        }
+
+        public string resultado()
+        {
+            if(mediaNotas() < 4)
+            {
+                return "Reprovado";
+            }
+            else if (mediaNotas() >= 4 && mediaNotas() < 6)
+            {
+                return "Exame Especial";
+            }
+            else
+            {
+                return "Aprovado";
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Projeto06.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno(7, 8, 10);
+
+
+            Console.WriteLine("Douglas, está com: " + aluno.mediaNotas() + " e está " + aluno.resultado());
+        }
+    }
+}
